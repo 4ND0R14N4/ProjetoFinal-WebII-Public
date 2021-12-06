d55ddc254851ec3aa0fdbab4d01fea14dfad1fd9
Leonardo Villela <leonardovillela.c@gmail.com>
2021-12-05 23:47:30 -0300

    comentarios adicionados

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -3,19 +3,45 @@ module.exports = app => {
   
     var router = require("express").Router();
   
-    // Create a new Tutorial
+    // Create Usuario
     router.post("/", usuario.create);
   
-    // Retrieve a single Tutorial with id
+    // Listar usuario
+    /* retorno: 
+      status 200
+      {
+        "nome": "Bruna",
+        "cpf": "12345678910",
+        "telefone": "11999995555",
+        "senha": "senha",
+        "data_nasc": "1997-05-05T03:00:00.000Z",
+        "email": "bruninhastucchi@hotmail.com"
+      }
+      ou
+      status 404
+      {
+        "message": "Usuario nao encontrado com o CPF 1234567891."
+      }
+    */
     router.get("/:id", usuario.findOne);
 
-    // Update a Tutorial with id
+    // Update usuario
     router.put("/:id", usuario.update);
   
-    // Delete a Tutorial with id
+    // Deletar usuario
     router.delete("/:id", usuario.delete);
   
     // Login do usuario
+    /*
+      {
+        "cpf": "xxxx"
+        "senha": "xxxx"
+      }
+      Retorno:
+      {
+        "success": true (ou false)
+      }
+    */
     router.post("/login", usuario.login);
 
     app.use('/api/usuario', router);
